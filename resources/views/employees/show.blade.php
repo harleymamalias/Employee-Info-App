414f2390ae8fe68765a98bcc33f23667016c4e85
Harley Mamalias <harleymamalias@gmail.com>
2024-07-04 10:25:31 +0800

    updated show blade to display photo url text

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -10,7 +10,7 @@
     <p>Name: {{ $employee->name }}</p>
     <p>Email: {{ $employee->email }}</p>
     @if($employee->photo)
-    <p>Photo URL: <a href="{{ Storage::disk('employee_photos')->url($employee->photo) }}">{{ $employee->name }}'s Photo</a></p>
+    <p>Photo URL: {{ Storage::disk('employee_photos')->url($employee->photo) }}</p>
     @else
     <p>No photo uploaded.</p>
     @endif
